1f913b4144f85b6c93fbe5613279b10e8735bf13
Aluno <aluno@edu.senac.br>
2024-09-05 21:32:05 -0300

    trabalho de rotas

--- inicio.py
+++ inicio.py
@@ -18,7 +18,58 @@ app = Flask(__name__)
 def alo_flask():
     return render_template("cadastro.html")
 
+@app.route('/romulo')
+def mostrar():
+    return ("romulo e o professor")
+
+@app.route('/walter')
+def ver_mensagem():
+    return "e top "
+
+@app.route ('/suzi')
+def mostrar_nome():
+    return "su legal"
+
+@app.route ("/gustavo")
+def ver_o_nome ():
+    return "gugu"
+
+@app.route('/daniel')
+def mostrar_2():
+     return render_template("daniel.html")
+
+@app.route ("/robson")
+def mostrar_3():
+    return "robson robson"
+
+@app.route ("/raposo")
+def mostar_4 ():
+    return render_template("raposo.html")
+    
+
+@app.route ("/gustavo")
+def mostar_5 ():
+    return render_template("gustavo.html")
+
+
+@app.route ("/robson")
+def mostar_6 ():
+    return render_template("robson.html")
+
+
+@app.route ("/suzane")
+def mostar_7 ():
+    return render_template("suzane.html")
+
+
+@app.route ("/vivi")
+def mostar_8 ():
+    return render_template("vivi.html")
+
+
+
+
+
+
 # executando um objeto flask com debug ativado (hot reload)
 app.run(debug=True)
-
-
